2b18c9fe8ee14f95dbb2b5b8730c98e232f2fb6e
saravanan-selvaraju <saravanan036@outlook.com>
2020-04-19 12:07:36 +0530

    Updates

---
+++ precache-manifest.312864d0a633a6fc6410e3a6ac9db2ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27ffbfc20bede52a77375e4157bfb53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "691a6b799a93a5857bc9",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "45b66d1aecbcab7da242",
+    "url": "/static/css/main.76fada13.chunk.css"
+  },
+  {
+    "revision": "691a6b799a93a5857bc9",
+    "url": "/static/js/2.8f63575a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8f63575a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45b66d1aecbcab7da242",
+    "url": "/static/js/main.c5f87316.chunk.js"
+  },
+  {
+    "revision": "51c07b96f2ef18a7f422",
+    "url": "/static/js/runtime-main.1f6087d1.js"
+  }
+]);
